98d7e4e325c339d9440d2df99873656bd2f7de89
LUBTFY <christian.tomelius@gmail.com>
2025-01-17 17:59:16 +0100

    Make background transparent

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 import Orb from "@/components/orb";
 
-// Removed unused imports
-// import Image from "next/image";
-// import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-black">
+    <main className="min-h-screen flex items-center justify-center" style={{ background: 'transparent' }}>
       <Orb />
     </main>
   );
